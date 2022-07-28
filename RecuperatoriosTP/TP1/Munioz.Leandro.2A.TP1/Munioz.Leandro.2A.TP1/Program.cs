bb70df56847195b8b68d48ea7f3975e8bbde8423
Leandro Muñoz <47321348+leandromunioz@users.noreply.github.com>
2022-07-28 00:45:42 -0300

    Add files via upload

---
+++ RecuperatoriosTP/TP1/Munioz.Leandro.2A.TP1/Munioz.Leandro.2A.TP1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Munioz.Leandro._2A.TP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
