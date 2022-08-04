3e1f881a910f6683eae0b81dfbbff8a29cc99191
Leandro Muñoz <47321348+leandromunioz@users.noreply.github.com>
2022-08-04 18:59:01 -0300

    Ultima revisión 18:58

---
+++ RecuperatoriosTP/RecuperatorioTP4/Clientes/ExcepcionDniInvalido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gimnasio
+{
+    public class ExcepcionDniInvalido: Exception
+    {
+        public ExcepcionDniInvalido(string mensaje) : this (null,mensaje)
+        {
+
+        }
+
+        public ExcepcionDniInvalido(Exception inner , string mensaje) : base (mensaje,inner)
+        {
+
+        }
+
+
+
+    }
+
+}
---
+++ RecuperatoriosTP/RecuperatorioTP4/Clientes/ISerializadora.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gimnasio
+{
+    public interface ISerializadora<T>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="elemento"></param>
+        /// <param name="nombreArchivo"></param>
+        public void Serializar(T elemento, string nombreArchivo);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nombreArchivo"></param>
+        /// <returns></returns>
+        public T Deserializar(string nombreArchivo);
+    }
+}
