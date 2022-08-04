980b86fa8f8915c7ed6bf465026f6efa8ab9c351
Leandro Muñoz <47321348+leandromunioz@users.noreply.github.com>
2022-08-04 12:12:59 -0300

    Recuperatorio_TPFINAL

---
+++ RecuperatoriosTP/RecuperatorioTP4/Clientes/Sistema.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gimnasio
+{
+    public class Sistema<T> where T : class
+    {
+
+        List<T> lista;
+
+        public Sistema()
+        {
+            lista = new List<T>();
+        }
+
+        public List<T> Lista
+        {
+            get
+            {
+                return lista;
+            }
+            set
+            {
+                lista = value;
+            }
+        }
+
+        public bool AgregarElemento(T e)
+        {
+            foreach (T item in lista)
+            {
+                if (e.Equals(item))
+                {
+                    return false;
+                }
+            }
+
+            lista.Add(e);
+            return true;
+
+        }
+
+
+
+        public override string ToString()
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (T item in lista)
+            {
+                sb.Append(item.ToString());
+            }
+
+            return sb.ToString();
+
+        }
+
+
+        public T BuscarElemento(T e)
+        {
+            foreach (T item in lista)
+            {
+                if (item.Equals(e))
+                {
+                    return item;
+                }
+            }
+
+            return null;
+
+        }
+
+
+
+
+    }
+}
---
+++ RecuperatoriosTP/RecuperatorioTP4/Gimnasio PAPUSA/Buscar cliente.cs
@@ -0,0 +1,92 @@
+﻿using Gimnasio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gimnasio_PAPUSA
+{
+    public partial class Buscar_cliente : Form
+    {
+        Sistema<Clientes> sistema;
+
+        public Buscar_cliente(Sistema<Clientes> sistema)
+        {
+            InitializeComponent();
+            this.sistema = sistema;
+        }
+
+        private void btn_buscar_dni_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_buscar_dni.Text))
+            {
+                MessageBox.Show("Complete el campo para buscar el cliente por su Dni.");
+            }
+
+       
+            else
+            {
+                Clientes cliente = sistema.BuscarElemento(new Clientes("Leandro", "Munoz", txt_buscar_dni.Text, new DateTime(1996, 12, 22), "Mendoza 555", DateTime.Today));
+
+                if (cliente is not null)
+                {
+                    Cliente_encontrado formClienteEncontrado = new Cliente_encontrado(cliente, sistema.Lista);
+                    this.Close();
+                    formClienteEncontrado.ShowDialog();
+
+                }
+                else
+                {
+                    MessageBox.Show("El Dni ingresado no se encuentra en el sistema.");
+                }
+
+            }
+        }
+
+        private void txt_buscar_dni_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+
+            if ((e.KeyChar > 31 && e.KeyChar < 48) || (e.KeyChar > 57 && e.KeyChar < 256))
+            {
+                MessageBox.Show("Este campo admite sólo números.", "¡Atención!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+
+            }
+            else
+            {
+                if (e.KeyChar == Convert.ToChar(Keys.Enter))
+                {
+                    Clientes cliente = sistema.BuscarElemento(new Clientes("a", "ape", txt_buscar_dni.Text, new DateTime(1996, 12, 22), "dd", DateTime.Today));
+
+                    if (cliente is not null)
+                    {
+                        Cliente_encontrado formClienteEncontrado = new Cliente_encontrado(cliente, sistema.Lista);
+                        this.Close();
+                        formClienteEncontrado.ShowDialog();
+
+                    }
+
+
+                }
+            }
+
+        }
+
+        private void Buscar_cliente_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_buscar_dni_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
